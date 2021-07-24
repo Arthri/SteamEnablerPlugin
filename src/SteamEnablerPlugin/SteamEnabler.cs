37e35abf2512011017b56b82d9d892d470c13ec2
Arthri <41360489+Arthri@users.noreply.github.com>
2021-07-24 18:07:25 +0800

    feat: Plugin template

---
+++ src/SteamEnablerPlugin/SteamEnabler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+using Terraria;
+using Terraria.Social;
+using TerrariaApi.Server;
+
+namespace SteamEnablerPlugin
+{
+    [ApiVersion(2, 1)]
+    public class SteamEnabler : TerrariaPlugin
+    {
+        public override string Name => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+        public override string Description => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override string Author => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+
+        public SteamEnabler(Main game) : base(game)
+        {
+        }
+    }
+}
