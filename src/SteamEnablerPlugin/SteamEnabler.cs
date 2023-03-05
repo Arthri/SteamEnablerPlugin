ae2d167b12b4a9ec106029e653fb46afbad7ce70
Arthri <41360489+a@users.noreply.github.com>
2023-03-05 18:12:38 +0000

    Use typeof(SteamEnabler).Assembly instead of get current assembly
    More performant

--- src/SteamEnablerPlugin/SteamEnabler.cs
+++ src/SteamEnablerPlugin/SteamEnabler.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Reflection;
 using Terraria;
 using Terraria.Social;
@@ -9,13 +9,13 @@ namespace SteamEnablerPlugin
     [ApiVersion(2, 1)]
     public class SteamEnabler : TerrariaPlugin
     {
-        public override string Name => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        public override string Name => typeof(SteamEnabler).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
 
-        public override string Description => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+        public override string Description => typeof(SteamEnabler).Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
 
-        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        public override Version Version => typeof(SteamEnabler).Assembly.GetName().Version;
 
-        public override string Author => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+        public override string Author => typeof(SteamEnabler).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
 
         public SteamEnabler(Main game) : base(game)
         {
